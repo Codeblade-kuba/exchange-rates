5fd386584935f040972cda3dd73ea22aad53b4fd
Jakub Chmielewski <kuba@codeblade.pl>
2022-09-03 19:12:23 +0200

    add testing environment

--- src/components/ResetAppButton/index.js
+++ src/components/ResetAppButton/index.js
@@ -6,7 +6,14 @@ import { appDefaultSettings } from '../../data/appDefaultSettings';
 const ResetAppButton = () => {
   const { setAppState } = useContext(AppContext);
 
-  return <button onClick={() => setAppState(appDefaultSettings)}>Reset</button>;
+  return (
+    <button
+      data-testid="reset-app-button"
+      onClick={() => setAppState(appDefaultSettings)}
+    >
+      Reset
+    </button>
+  );
 };
 
 export default ResetAppButton;
